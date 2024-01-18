247447c413a3729acd9ba6a50c5df9bdd8904d5c
Jacek Ostański <jacek@ivolution.pl>
2024-01-18 18:44:06 +0100

    lifecycle rule

---
+++ s3-lifecycle.tf
@@ -0,0 +1,8 @@
+resource "aws_ssm_document" "s3_lifecycle" {
+  name            = "ConfigureS3BucketLifecycleRule"
+  document_type   = "Automation"
+  document_format = "YAML"
+
+  content = templatefile("${path.module}/ConfigureS3BucketLifecycleRule.yaml", {})
+
+}
